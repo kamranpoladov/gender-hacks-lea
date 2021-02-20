275c54a09f4de7b27b3475e32b7aefe111b40ef1
rafaelsofizadeh <rafa.sofizadeh@gmail.com>
2021-02-20 23:30:43 +0400

    Working map component

--- src/client/components/MainPage/MainPage.tsx
+++ src/client/components/MainPage/MainPage.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@material-ui/core';
 import { useGetSelectedHostLocation } from '../../hooks';
 import { HostsList } from '../HostsList';
+import { Map } from '../Map';
 
 export const MainPage = () => {
   const {
@@ -10,6 +11,9 @@ export const MainPage = () => {
 
   return (
     <Box style={{ position: 'relative' }}>
+      <Box style={{ position: 'absolute', top: '0' }}>
+        <Map />
+      </Box>
       <Box style={{ position: 'absolute', top: '400px' }}>
         <HostsList setSelectedHostLocation={setSelectedHostLocation} />
       </Box>
